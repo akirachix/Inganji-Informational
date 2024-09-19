affcc514acadd721af6f39d865d9fddb94408c0d
Ivy-Wanjiku <wanjiruwanjikuivy@gmail.com>
2024-09-19 19:03:03 +0300

    update about

--- fanikisha/src/app/page.tsx
+++ fanikisha/src/app/page.tsx
@@ -1,16 +1,6 @@
 
 import React from 'react';
 import Footer from './Footer';
-
-const Home: React.FC = () => {
-  return (
-    <div>
-    <Footer/>
-    </div>
-  );
-};
-
-export default Home;
 import Product from "./Product"
 import About from "./About"
 
@@ -19,6 +9,7 @@ export default function Home(){
     <main>
       <About/>
     <Product/>
+    <Footer/>
     </main>
   )
 }
